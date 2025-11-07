03f917559bc8c31bac8c8994b64ba958bb6d8919
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-11-07 17:41:43 +0100

    Header styling, responsiveness, and linking done. Just need to do some more CSS work with showing what link is active

---
+++ app/Header.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="bg-white px-5 py-2 flex justify-between items-center sticky top-0 shadow">
+            <Link href="/">
+                <Image
+                    src="/mobile-logo.svg"
+                    width={40}
+                    height={40}
+                    alt="PrintForge logo mobile"
+                    className="sm:hidden"
+                />
+                <Image
+                    src="/desktop-logo.svg"
+                    width={240}
+                    height={61}
+                    alt="PrintForge logo desktop"
+                    className="hidden sm:block"
+                />
+            </Link>
+            <nav>
+                <ul className="flex items-center gap-5 text-gray-600">
+                    <li>
+                        <Link href="/models" className="uppercase text-sm font-semibold tracking-wider">3D models</Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className="uppercase text-sm font-semibold tracking-wider">About</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
