e4f20edb4ad1171900ffc36677ef4df2ddd63887
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-11-07 17:40:34 +0100

    Removed the import and usage of the Montserrat Alternates font by using it in a much easier way with a variable and using that variable in the globals.css file

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { montserrat, albertSans } from "./layout";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -7,12 +6,12 @@ export default function HomePage() {
         <main>
             <section className="my-10 px-5 flex flex-col sm:flex-row sm:items-center sm:gap-25 sm:px-10">
                 <div className="mb-10 max-w-[600px]">
-                    <p className={`hidden mb-5 uppercase font-medium tracking-wider sm:block`}>Your go-to platform for 3D printing files</p>
-                    <h1 className={`${montserrat.className} mb-5 font-bold text-[2.5rem] sm:text-[3.5rem]`}>Discover what's possible with 3D printing</h1>
+                    <p className="hidden mb-5 uppercase font-medium tracking-wider sm:block">Your go-to platform for 3D printing files</p>
+                    <h1 className="mb-5 font-bold text-[2.5rem] sm:text-[3.5rem]">Discover what's possible with 3D printing</h1>
                     <p className="mb-10 text-2xl sm:text-[1.75rem] sm:mb-18">Join our community of creators and explore a vast library of user-submitted models.</p>
                     <Link
                         href="/models"
-                        className={`p-3 uppercase text-xl ${albertSans.className} font-bold border-2 border-black sm:font-semibold`}
+                        className={`p-3 uppercase text-xl font-bold border-2 border-black sm:font-semibold`}
                     >Browse models</Link>
                 </div>
                 <Image
