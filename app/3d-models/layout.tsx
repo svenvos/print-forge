d421691490e57abde44d9be5bbd7cb55cc5816cd
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-11-10 19:38:25 +0100

    Small CSS change

--- app/3d-models/layout.tsx
+++ app/3d-models/layout.tsx
@@ -2,7 +2,7 @@ import CategoriesNavbar from "@/app/components/CategoriesNavbar";
 
 export default function ModelsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <main className="py-10 flex flex-col gap-10 sm:flex-row sm:gap-20">
+        <main className="mb-10 flex flex-col gap-5 relative sm:flex-row sm:gap-20 sm:my-10">
             <CategoriesNavbar />
             <div className="max-w-[1212px] mx-auto px-5 sm:px-0 sm:pr-10">
                 {children}
