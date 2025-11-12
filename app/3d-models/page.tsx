e67487c0fb1bde0d456099f3ba1930695491f4ee
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-11-12 18:32:25 +0100

    Added a search form function for filtering the 3d models

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,3 +1,4 @@
+import Form from "next/form";
 import { getModels } from "@/app/lib/models";
 import ModelsGrid from "../components/ModelsGrid";
 
@@ -11,10 +12,37 @@ export type Model = {
     dateAdded: string
 };
 
-export default async function ModelsPage() {
+type ModelsPageProps = {
+    searchParams: {
+        q?: string
+    }
+};
+
+export default async function ModelsPage({ searchParams }: ModelsPageProps) {
     const models: Model[] = await getModels();
+    const { q } = await searchParams;
+
+    const filteredModels: Model[] = q
+    ? models.filter((model: Model) => 
+        model.name.toLowerCase().includes(q.toLowerCase()) ||
+        model.description.toLowerCase().includes(q.toLowerCase()
+    ))
+    : models;
 
     return (
-        <ModelsGrid title="All 3D Models" models={models} />
+        <>
+            <Form action="/3d-models" className="flex flex-col mb-8">
+                <input
+                    className="px-4 py-2 w-full border border-[#606060] rounded-full sm:w-[300px] sm:self-end"
+                    type="text"
+                    name="q"
+                    placeholder="Search for a model..."
+                    autoComplete="off"
+                    defaultValue={q}
+                    aria-label="Search for specific models."
+                />
+            </Form>
+            <ModelsGrid title="All 3D Models" models={filteredModels} />
+        </>
     );
 }
